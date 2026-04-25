c2c1a28df94f2ae9944aff78bf9ad97678c4f2ca
Nitin Sonu <nsdotdev@gmail.com>
2026-04-25 14:20:53 +0545

    content update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ export default function About({ onNavigate }) {
                   <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/>
                   <circle cx="12" cy="10" r="3"/>
                 </svg>
-                Kathmandu, Nepal
+                Lalitpur, Nepal
               </span>
             </div>
 
